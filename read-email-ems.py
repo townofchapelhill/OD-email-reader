e746b63a9989b5e3fff497955247c2470a0c20b3
pcolar <dave@pcolar.com>
2019-10-02 11:39:29 -0400

    tweak field detection and response

--- read-email-ems.py
+++ read-email-ems.py
@@ -4,10 +4,11 @@ import email
 import secrets, filename_secrets
 import PDC_lookup
 import pathlib
-import os, csv, re, datetime, traceback, datetime, ssl
-import pandas as pd
-from pandas.errors import ParserError
-import logging
+import os, csv, re, datetime, traceback, datetime
+#import ssl
+#import pandas as pd
+#from pandas.errors import ParserError
+#import logging
 
 # Search Parameters
 OPS_code = re.compile(r'OPS\d')
@@ -38,16 +39,20 @@ def etl_data(server):
     # Create Path to staging directory
     stagingPath = pathlib.Path(filename_secrets.productionStaging)
     # Open csv
-    outputFilename = stagingPath.joinpath('fire_dept_raw_dispatches_test.csv')
-    info_sheet = open(outputFilename, 'w')
+    outputFilename = stagingPath.joinpath('fire_dept_dispatches.csv')
+    outputFile = open(outputFilename, 'a+')
+    csv_header = ["CAD","Address","City","Type of Incident","Date","PDC"]
+    info_sheet = csv.writer(outputFile)
+    # If there's no header, write headers
+    if os.stat(outputFilename).st_size == 0:
+        info_sheet.writerow(csv_header)
     # Selects inbox as target
     server.select(mailbox='Inbox')
     # Select emails since yesterday
     yesterday = datetime.date.today() - datetime.timedelta(days=7)
     searchQuery = '(FROM "Orange Co EMS Dispatch" SINCE "' + yesterday.strftime('%d-%b-%Y') + '")'
-    # If there's no header, write headers
-    if os.stat(outputFilename).st_size == 0:
-        info_sheet.write("CAD,City,Type of Incident\n")
+    # test for all emails below
+    #searchQuery = '(FROM "Orange Co EMS Dispatch")'
 
     result, data = server.uid('search', None, searchQuery)
     message_list = data[0].split()
@@ -67,113 +72,66 @@ def etl_data(server):
         email_date = datetime.datetime.strptime(email_message._headers[7][1][5:], '%d %b %Y %X %z')
 
         # output list
-        output_fields = [None]*5
+        output_fields = [None]*6
         # fetch text content
         split_text = email_message._payload.split(";")
         # Input format is expected to be
         # CAD;Address;City;Text Desc;OPS;PDC
-
-        output_fields[0] = re.sub(extraneous_chars, '', split_text[0])
-        output_fields[1] = re.sub(extraneous_chars, '', split_text[1])
-        output_fields[2] = re.sub(extraneous_chars, '', split_text[2])
-       
-        textDescription = "UNKNOWN/UNSPECIFIED"
         try:
-            if len(split_text) < 5:
-                # no codes
-                if re.match(ems_match, split_text[3]):
-                    textDescription = "EMS"
-                else:
-                    textDescription = re.sub(extraneous_chars, '', split_text[3])
-
-            if len(split_text) > 4:
-                # OPS code
-                split_text[4]    = re.sub(extraneous_chars, '', split_text[4])
-                if re.match(OPS_code, split_text[4]):
-                    if split_text[4] == "OPS1":
+            output_fields[0] = re.sub(extraneous_chars, '', split_text[0])
+            output_fields[1] = re.sub(extraneous_chars, '', split_text[1])
+            output_fields[2] = re.sub(extraneous_chars, '', split_text[2])
+       
+            try:
+                textDescription = re.sub(extraneous_chars, '', split_text[3])
+                if len(split_text) < 5:
+                    # no codes
+                    if re.match(ems_match, split_text[3]):
                         textDescription = "EMS"
-                    elif split_text[4] == "OPS2":
-                        textDescription = "FIRE"     
+                    else:
+                        textDescription = re.sub(extraneous_chars, '', split_text[3])
 
-            if len(split_text) > 5:
-                # PDC 
-                split_text[5]    = re.sub(extraneous_chars, '', split_text[5])
-                if re.match(PDC_code, split_text[5]):
-                    PDC_match = re.split(r'[A-Z]',  split_text[5], maxsplit=1)
-                    if int(PDC_match[0]) > 0 and int(PDC_match[0]) < 50:
-                        textDescription = "EMS"
-                    elif int(PDC_match[0]) > 50 and int(PDC_match[0]) < 100:
-                        textDescription = PDC_lookup.PDC[int(PDC_match[0])]
-                        #textDescription = "FIRE"
-                        #textDescription = re.sub(extraneous_chars, '', split_text[3])
-                    elif int(PDC_match[0]) > 100:
-                        textDescription = "POLICE"
-                        #textDescription = re.sub(extraneous_chars, '', split_text[3])
-                    #textDescription = PDC_lookup.PDC[int(PDC_match[0])]
-    
-        except (IndexError, KeyError):
-            textDescription = re.sub(extraneous_chars, '', split_text[3])
-        output_fields[3] = textDescription.upper()
-        output_fields[4] = datetime.datetime.strftime(email_date, '%Y-%m-%d %H:%M')
+                if len(split_text) > 4:
+                    # OPS code
+                    split_text[4]    = re.sub(extraneous_chars, '', split_text[4])
+                    if re.match(OPS_code, split_text[4]):
+                        if split_text[4] == "OPS1":
+                            textDescription = "EMS"
+                        elif split_text[4] == "OPS2":
+                            textDescription = "FIRE"     
 
-        # Convert item to string
-        output_string = str(output_fields)
-        # Clean string up and write data to csv
-        print(output_string)
-        info_sheet.write(output_string + "\n")
-    # Close CSV being used
-    info_sheet.close()
-    # Call cleanup_csv function using dates list
-    #cleanup_csv(datetimes)
-    # Call logout function
+                if len(split_text) > 5:
+                    # PDC 
+                    split_text[5] = re.sub(extraneous_chars, '', split_text[5])
+                    if re.match(PDC_code, split_text[5]):
+                        output_fields[5] = split_text[5]
+                        PDC_match = re.split(r'[A-Z]',  split_text[5], maxsplit=1)
+                        if int(PDC_match[0]) > 0 and int(PDC_match[0]) < 50:
+                            textDescription = "EMS"
+                        elif int(PDC_match[0]) > 50 and int(PDC_match[0]) < 100:
+                            textDescription = PDC_lookup.PDC[int(PDC_match[0])]
+                            #textDescription = "FIRE"
+                            #textDescription = re.sub(extraneous_chars, '', split_text[3])
+                        elif int(PDC_match[0]) > 100:
+                            textDescription = "POLICE"
+                            #textDescription = re.sub(extraneous_chars, '', split_text[3])
+                        #textDescription = PDC_lookup.PDC[int(PDC_match[0])]
+        
+            except (IndexError, KeyError):
+                textDescription = "UNKNOWN/UNSPECIFIED"
+            output_fields[3] = textDescription.upper()
+            output_fields[4] = datetime.datetime.strftime(email_date, '%Y-%m-%d %H:%M')
+            
+            #output_string = str(output_fields)
+            # Clean string up and write data to csv
+            print(output_fields)
+            info_sheet.writerow(output_fields)
+        except IndexError:
+            pass
+    # Close CSV
+    outputFile.close()
     server.logout()
 
-# Function to clean up CSV that is created
-def cleanup_csv(dateslist):
-    print("Cleaning CSV...")
-    # Create Path to staging directory
-    stagingPath = pathlib.Path(filename_secrets.productionStaging)
-    csvFilename = stagingPath.joinpath('fire_dept_raw_dispatches.csv')
-    # Create pandas dataframe from original csv
-    df = pd.read_csv(csvFilename)
-    # , error_bad_lines=False, warn_bad_lines=True
-
-    # Delete PII rows and drop duplicate records
-    del df["ID"]
-    del df["ID2"]
-    df['Dates'] = pd.Series(dateslist, index = df.index[:len(dateslist)])
-    df.drop_duplicates(keep='first')
-    print(df)
-    outputFilename = stagingPath.joinpath('fire_dept_dispatches_clean.csv')
-    clean_file = open(outputFilename, "a+")
-    # Write headers to blank clean file
-    if os.stat(outputFilename).st_size == 0:
-        clean_file.write(",CAD,Address,City,Type of Incident,Dates\n")
-    # df.drop("Address", axis=1, inplace=True)
-
-    # Write dataframe to new, finalized csv
-    df.to_csv(clean_file, mode='w', header=False)
-    print("CSV cleaned and rewritten.")
-
-
-# Main function to call other functions
-def main():
-
-    # Open log file
-    #logfilePath = pathlib.Path(filename_secrets.logfilesDirectory)
-    #fireLog = logfilePath.joinpath('fire_dispatch_log.txt')
-    #fire_log = open(fireLog, "a+")
-    # try:
-        # Set var to hold what is returned from long_and_write()
-    exchange_mail = IMAP_login()
-        # Call etl_data() using the exchange imap server
-    etl_data(exchange_mail)
-        # Log failures and successes
-    #fire_log.write(str(datetime.datetime.now()) + "\n" + "Logged into Exchange IMAP and saved emails." + "\n")
-    # except:
-        # fire_log.write(str(datetime.datetime.now()) + "\n" + "There was an issue when trying to log in and save emails." + "\n" + traceback.format_exc() + "\n")
-
-# Call main
-#main()
+# Main
 exchange_mail = IMAP_login()
 etl_data(exchange_mail)
